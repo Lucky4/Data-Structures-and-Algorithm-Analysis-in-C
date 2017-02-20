3e8f9e41ed6042e376c9a37dd7e976b1754f255c
lucky4 <591398706@qq.com>
2017-02-20 08:12:00 +0800

    增加了堆排序

---
+++ 排序/堆排序.cpp
@@ -0,0 +1,77 @@
+/*
+堆排序
+
+思路：堆排序分为构建堆、删除最值、调整堆即上滤下滤操作。
+
+方法：使用数组代替ADT（抽象数据类型）的方式实现初始堆数组下标从零开始。
+
+总结：时间复杂度为N*logN。
+*/
+
+//上滤操作
+void PercDown(ElementType A[], int i, int N)	//i为父节点下标
+{
+	int replChild;		//上滤操作时用来与根节点交换的孩子节点下标
+	ElementType tmp;	//一般利用交换进行的排序都要有一个tmp变量
+
+	//每次开始都要取出最顶点下标，所以可以写在for语句头中作为初始条件
+	//遍历的条件就是父节点还有孩子
+	//执行完此次上滤操作后需要找到下次上滤操作的父节点，所以我们也可以写到for语句头
+	for (tmp = A[i]; 2*i+1 < N; i = replChild)	//i = replChild是在语句体执行之后执行的
+	{
+		if (2 * i + 1 != N - 1 && A[2 * i + 1] < A[2 * i + 1 + 1])//若右孩子存在且右孩子大则将右孩子上滤
+			replChild = 2 * i + 1 + 1;
+		if (2 * i + 1 != N - 1 && A[2 * i + 1] > A[2 * i + 1 + 1])//若右孩子存在且右孩子小则将左孩子上滤
+			replChild = 2 * i + 1;
+		if(2 * i + 1 == N - 1)		//若右孩子不存在则将左孩子上滤
+				replChild = 2 * i + 1;
+
+		//书上方法
+		//replChild = 2*i+1;
+		//if (replChild != N - 1 && A[replChild + 1] > A[replChild])
+		//	replChild++;
+
+		//满足孩子比父亲数据大,将孩子数据赋值父亲数据,不需要上滤跳出循环
+		if (tmp < A[replChild])
+			A[i] = A[replChild];
+		else
+			break;
+	}
+	A[i] = tmp;
+}
+
+//堆排序
+void HeapSort(ElementType A[], int N)
+{
+	int i, temp;;
+
+	for (i = N / 2; i >= 0; i--)
+		PercDown(A, i, N);
+	for (i = N - 1; i > 0; i--)
+	{
+		temp = A[0];
+		A[0] = A[i];
+		A[i] = temp;
+
+		PercDown(A, 0, i);
+	}
+}
+
+
+
+
+
+
+
+int main()
+{
+	int i;
+	int a[10] = { 2, 4, 67, 12, 43, 78, 5, 3, 8, 65 };
+
+	HeapSort(a, 10);
+	for (i = 0; i < 10; i++)
+		printf("%d,", a[i]);
+
+    return 0;
+}
+
