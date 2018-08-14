73ab73d2884cc0bfcc56de7462cd5145e03384dc
lucky4 <591398706@qq.com>
2018-08-15 03:48:13 +0800

    update

--- 排序/堆排序.cpp
+++ 排序/堆排序.cpp
@@ -11,29 +11,16 @@
 //下滤操作
 void PercDown(ElementType A[], int i, int N)	//i为父节点下标
 {
-	int replChild;		//下滤操作时用来与根节点交换的孩子节点下标
-	ElementType tmp;	//一般利用交换进行的排序都要有一个tmp变量
+	int replChild;
+	ElementType tmp;
 
-	//每次开始都要取出最顶点下标，所以可以写在for语句头中作为初始条件
-	//遍历的条件就是父节点还有孩子
-	//执行完此次下滤操作后需要找到下次下滤操作的父节点，所以我们也可以写到for语句头
 	for (tmp = A[i]; 2*i+1 < N; i = replChild)	//i = replChild是在语句体执行之后执行的
 	{
-		if (2 * i + 1 != N - 1 && A[2 * i + 1] < A[2 * i + 1 + 1])//若右孩子存在且右孩子大则将右孩子下滤
-			replChild = 2 * i + 1 + 1;
-		if (2 * i + 1 != N - 1 && A[2 * i + 1] > A[2 * i + 1 + 1])//若右孩子存在且右孩子小则将左孩子下滤
-			replChild = 2 * i + 1;
-		if(2 * i + 1 == N - 1)		//若右孩子不存在则将左孩子下滤
-				replChild = 2 * i + 1;
+		replChild = 2*i;
+		if (replChild != N - 1 && A[replChild + 1] < A[replChild])
+			replChild++;
 
-		//书上方法
-		//replChild = 2*i+1;
-		//if (replChild != N - 1 && A[replChild + 1] > A[replChild])
-		//	replChild++;
-
-		//满足孩子比父亲数据大,将孩子数据赋值父亲数据,
-		//否则，不满足堆结构，不需要下滤跳出循环
-		if (tmp < A[replChild])
+		if (tmp > A[replChild])
 			A[i] = A[replChild];
 		else
 			break;
